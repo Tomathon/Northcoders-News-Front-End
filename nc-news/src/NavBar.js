65ceecbde6dd248b7c875dff5d5869aa4e4607cd
tomathon <tomathonmiller@gmail.com>
2018-03-03 16:50:59 +0000

    Add nav links to articles by topic

--- nc-news/src/NavBar.js
+++ nc-news/src/NavBar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
-class Nav extends Component {
+class Navbar extends Component {
   render() {
     return (
-      
+      <nav>
+          <NavLink to="/">Home</NavLink>
+          {this.props.topics.map((topic, i) => {
+            return (
+              <a key={i} href={`/topics/${topic.slug}/articles`}>{topic.title}</a>
+            )
+          })}
+      </nav>
     )
   }
 }
+
+export default Navbar;
