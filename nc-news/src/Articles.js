4795ec4a87b35f71a5c8e19601555c4617069652
tomathon <tomathonmiller@gmail.com>
2018-03-03 15:14:02 +0000

    Add shortenStr function to Articles component

--- nc-news/src/Articles.js
+++ nc-news/src/Articles.js
@@ -29,7 +29,7 @@ class Articles extends Component {
               <div key={i}>
                       <h3>{article.title}</h3>
                       <p>Author: {article.created_by}</p>
-                      <p>{article.body}</p>
+                      <p>{this.shortenStr(article.body)}</p>
                       <p>Topic: {article.belongs_to}</p>
                       <p>Votes: {article.votes}</p>
               </div>
@@ -37,6 +37,15 @@ class Articles extends Component {
           </div>
       </div>
     )
+  }
+
+  shortenStr = str => {
+    let spaces = []
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' ') spaces.push(i);
+    }
+    let sliceIndex = spaces[Math.round((spaces.length - 1) / 2)];
+    return str.slice(0, sliceIndex) + '...';
   }
 
 }
