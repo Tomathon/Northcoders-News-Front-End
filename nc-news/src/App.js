19a8077b180238011f1582bca0f3abc7c861aef6
tomathon <tomathonmiller@gmail.com>
2018-03-03 11:49:12 +0000

    Add getTopics function

---
+++ nc-news/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+
+class App extends Component {
+  
+  state = {
+    topics: []
+  }
+  
+  componentDidMount() {
+    this.getTopics()
+  }
+
+  getTopics = () => {
+    fetch('https://pure-thicket-72217.herokuapp.com/api/topics')
+      .then(buffer => buffer.json())
+      .then(res => {
+        this.setState({
+          topics: res.topics
+        })
+      })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Northcoders News</h1>
+          </header>
+          <div>
+            <Route exact path='/' render={() => <Home topics={this.state.topics}/>}/>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
