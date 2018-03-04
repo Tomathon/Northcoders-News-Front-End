b13fc06b7029ca4655655c3fbc27bf443a148827
tomathon <tomathonmiller@gmail.com>
2018-03-04 09:36:09 +0000

    Add Links and function

--- nc-news/src/Article.js
+++ nc-news/src/Article.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Article extends Component {
 
@@ -7,12 +8,13 @@ class Article extends Component {
     body: '',
     created_by: '',
     belongs_to: '',
-    comments: 0,
+    comments: [],
     votes: 0
   }
 
   componentDidMount() {
     this.getArticleById(this.props.match.params.article_id)
+    this.getArticleCommentsById(this.props.match.params.article_id)
   }
 
   getArticleById = article_id => {
@@ -20,6 +22,7 @@ class Article extends Component {
       .then(buffer => buffer.json())
       .then(res => {
         this.setState({
+          _id: res.article._id,
           title: res.article.title,
           body: res.article.body,
           created_by: res.article.created_by,
@@ -29,15 +32,25 @@ class Article extends Component {
       })
   }
 
+  getArticleCommentsById = article_id => {
+    fetch(`https://pure-thicket-72217.herokuapp.com/api/articles/${article_id}/comments`)
+      .then(buffer => buffer.json())
+      .then(res => {
+        this.setState({
+          comments: res.comments
+        })
+      })
+  }
+
   render() {
     return (
       <div>
         <h2>{this.state.title}</h2>
         <p>{this.state.body}</p>
-        <p>Created by: {this.state.created_by}</p>
-        <p>Topic: {this.state.belongs_to}</p>
+        <p>Created by: <Link to={`/users/${this.state.created_by}`}>{this.state.created_by}</Link></p>
+        <p>Topic: <Link to={`/topics/${this.state.belongs_to}/articles`}>{this.state.belongs_to}</Link></p>
         <p>Votes: {this.state.votes}</p>
-        <p>Comments: {this.state.comments.length}</p> 
+        <p>Comments: <Link to={`${this.state._id}/comments`}>{this.state.comments.length}</Link></p>
       </div>
     )
   }
