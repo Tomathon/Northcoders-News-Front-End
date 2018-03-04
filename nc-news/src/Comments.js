f5837305fba9f8f8b8736f504c69b737dd9ecf7b
tomathon <tomathonmiller@gmail.com>
2018-03-04 09:38:02 +0000

    Add function and update path

--- nc-news/src/Comments.js
+++ nc-news/src/Comments.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Comments extends Component {
 
   state = {
+    article: {},
     comments: []
   }
 
   componentDidMount = article_id => {
     this.getArticleComments(this.props.match.params.article_id)
+    this.getArticle(this.props.match.params.article_id)
   }
 
   getArticleComments = article_id => {
     fetch(`https://pure-thicket-72217.herokuapp.com/api/articles/${article_id}/comments`)
       .then(buffer => buffer.json())
       .then(res => {
-        console.log(res)
         this.setState({
           comments: res.comments
+        })
+      })
+  }
+
+  getArticle = article_id => {
+    fetch(`https://pure-thicket-72217.herokuapp.com/api/articles/${article_id}`)
+      .then(buffer => buffer.json())
+      .then(res => {
+        this.setState({
+          article: res.article
         })
       })
   }
@@ -24,6 +36,7 @@ class Comments extends Component {
   render() {
     return (
       <div>
+        <p><Link to={`/articles/${this.state.article._id}`}>Back to Article</Link></p>
         {this.state.comments.map((comment, i) => {
           return (
               <article key={i}>
