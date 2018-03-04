faf09aa2a1f2e84b65d4167483f982bf118261b8
tomathon <tomathonmiller@gmail.com>
2018-03-04 14:49:46 +0000

    Add ability to add comment

--- nc-news/src/Comments.js
+++ nc-news/src/Comments.js
@@ -5,12 +5,40 @@ class Comments extends Component {
 
   state = {
     article: {},
-    comments: []
+    comments: [],
+    comment: {},
+    value: ''
   }
 
   componentDidMount = article_id => {
     this.getArticleComments(this.props.match.params.article_id)
     this.getArticle(this.props.match.params.article_id)
+  }
+
+  handleChange = event => {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.value)
+    const comment = this.state.value;
+    console.log(comment)
+    this.postComment(comment);
+  }
+
+  postComment = comment => {
+    fetch(`https://pure-thicket-72217.herokuapp.com/api/articles/${this.props.match.params.article_id}/comments`, {
+      method: 'POST',
+      body: JSON.stringify({
+        comment: comment
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(body => {
+        this.setState({value: ''})
+        this.getArticleComments(this.props.match.params.article_id);
+      });
   }
 
   getArticleComments = article_id => {
@@ -37,12 +65,21 @@ class Comments extends Component {
     return (
       <div>
         <p><Link to={`/articles/${this.state.article._id}`}>Back to Article</Link></p>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="addComment">Add your comment</label>
+            <div>
+              <textarea value={this.state.value} onChange={this.handleChange} placeholder="Enter Comment"></textarea>
+            </div>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
         {this.state.comments.map((comment, i) => {
           return (
               <article key={i}>
-              <p>{comment.body}</p>
-              <p>User: <Link to={`/users/${comment.created_by}`}>{comment.created_by}</Link></p>
-              <p>Votes: {comment.votes}</p>
+                <p>{comment.body}</p>
+                <p>User: <Link to={`/users/${comment.created_by}`}>{comment.created_by}</Link></p>
+                <p>Votes: {comment.votes}</p>
               </article>
           )
         })}
