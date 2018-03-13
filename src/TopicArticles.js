b1725bcc19783a31f5406817e2bbea1e2bd8720a
tomathon <tomathonmiller@gmail.com>
2018-03-13 19:36:30 +0000

    Refactor updateArticleVotes

--- src/TopicArticles.js
+++ src/TopicArticles.js
@@ -21,6 +21,22 @@ class TopicArticles extends Component {
       })
   }
 
+  updateArticleVotes = (article_id, vote) => {
+    fetch(`https://pure-thicket-72217.herokuapp.com/api/articles/${article_id}?vote=${vote}`, { 
+      method: "PUT"
+    })
+      .then(buffer => buffer.json())
+      .then(res => {
+        this.state.articles.forEach(article => {
+          if (article._id === article_id && vote === 'up') article.votes += 1;
+          else if (article._id === article_id && vote === 'down') article.votes -=1;
+        });
+        this.setState({
+          articles: this.state.articles
+        });
+      })
+  }
+
   render() {
     return (
       <div className="Topic-articles">
@@ -33,7 +49,9 @@ class TopicArticles extends Component {
                 </Link>
                   <p>Author: <Link to={`/users/${article.created_by}`}>{article.created_by}</Link></p>
                   <p><Link to={`/articles/${article._id}/comments`}>Comments</Link></p>
-                  <p>Votes: {article.votes}</p>
+                  <i className="fa fa-thumbs-up" aria-hidden="true" onClick={() => this.updateArticleVotes(article._id, 'up')}></i>
+                  <p className="Vote">Votes: {article.votes}</p>
+                  <i className="fa fa-thumbs-down" aria-hidden="true" onClick={() => this.updateArticleVotes(article._id, 'down')}></i>
               </div>
             )
         })}
